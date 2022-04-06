7439bc5c6e2a203690882612182f430e229ab278
I-SMAF <ikarashov@gmail.com>
2022-04-06 16:24:49 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -4,13 +4,6 @@ from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.views import View
 
-from .models import *
-
-
-class MapView(View):
-    def get(self, request):
-        return render(request, "Map.html", {"title": "Map", "pages":pages()})
-
 
 def pages():
     pages = ["About", "Region", "Results", "Archive"]
@@ -25,6 +18,14 @@ def pages():
     return result
 
 
+from .models import *
+
+
+class MapView(View):
+    def get(self, request):
+        return render(request, "Map.html", {"title": "Map", "pages": pages()})
+
+
 class StaticView(View):
     def get(self, request):
         page = request.__dict__['resolver_match'].url_name.capitalize()
@@ -32,7 +33,7 @@ class StaticView(View):
         try:
             return render(request, "Static.html",
                           {"title": page, "heading": page_class._meta.original_attrs['verbose_name_plural'],
-                           "object": page_class.objects.get(status=True), "pages":pages()} )
+                           "object": page_class.objects.get(status=True), "pages": pages()})
         except:
             # TODO: refactorvc
             return HttpResponseNotFound(f'<h1 style="text-align: center;">{page} page doesn`t exists or not activated</h1>')
@@ -44,7 +45,7 @@ class SetView(View):
         page_class = getattr(sys.modules[__name__], page)
         try:
             return render(request, page + ".html",
-                          {"title": page, "heading": page_class._meta.verbose_name_plural, "objects": page_class.objects.all(), "pages":pages()})
+                          {"title": page, "heading": page_class._meta.verbose_name_plural, "objects": page_class.objects.all(), "pages": pages()})
         except:
             # TODO: refactor
             return HttpResponseNotFound(f'<h1 style="text-align: center;">{page} objects doesn`t exists</h1>')
@@ -54,7 +55,7 @@ class EventView(View):
     def get(self, request, id):
         try:
             event = Event.objects.get(id=id)
-            return render(request, "CurrentEvent.html", {"title": event.name, "object": event, "pages":pages()})
+            return render(request, "CurrentEvent.html", {"title": event.name, "object": event, "pages": pages()})
         except:
             # TODO: refactor
             return HttpResponseNotFound(f'<h1 style="text-align: center;">Event object with id = {id} doesn`t exists</h1>')
