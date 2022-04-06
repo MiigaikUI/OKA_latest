6f452900d273e4e31e44872cfbcfc787d5189ae4
AlexanderMalenkov <malenkov2001@gmail.com>
2022-04-06 15:58:54 +0300

    fix menu for galina blyta nahui

--- templates/static/script.js
+++ templates/static/script.js
@@ -1,22 +1,10 @@
-const details = document.querySelectorAll("details");
 
-details.forEach((targetDetail) => {
-    targetDetail.addEventListener("click", () => {
-        // Close all the details that are not targetDetail.
-        details.forEach((detail) => {
-            if (detail !== targetDetail) {
-                detail.removeAttribute("open");
-            }
-        });
-    });
-});
+document.getElementById("mySidenav").style.width = "0";
 
 function menuOpen() {
-    var menuBLock = document.querySelector('.menu-block');
-    var menu = document.querySelectorAll(".side-menu");
-    var btn = document.querySelector(".menu-open-button");
-    menuBLock.classList.toggle('block-opened');
-    for (i = 0; i <= menu.length; i++) {
-        menu[i].classList.toggle("open");
-    }
+  if (document.getElementById("mySidenav").style.width == "180px") {
+    document.getElementById("mySidenav").style.width = "0";
+  } else {
+    document.getElementById("mySidenav").style.width = "180px";
+  }
 }
